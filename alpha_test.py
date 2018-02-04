d6a21644f4195455d6dfd72855829f81c6478af8
Brian Sam Thomas <BRAiNCHiLD95@github.com>
2018-02-04 22:25:05 +0530

    final "fully working" version of alpha_test.py

--- alpha_test.py
+++ alpha_test.py
@@ -10,12 +10,11 @@ from skimage.io import imsave
 import cv2
 from keras.layers import Conv2D, UpSampling2D, InputLayer
 from keras.models import Sequential
+from keras.preprocessing.image import img_to_array, load_img
 
 # load image
-image = np.array(cv2.imread('test_images/im1.jpg', 1))
-cv2.imshow('title', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+image = img_to_array(load_img('test_images/im4.jpg'))
+image = np.array(image, dtype=float)
 # Convert to LAB and split L, ab
 L_chan = rgb2lab(1.0/255*image)[:,:,0]
 ab_chan = rgb2lab(1.0/255*image)[:,:,1:]
@@ -28,7 +27,7 @@ ab_chan = ab_chan.reshape(1, length, width, 2)
 
 #the alpha neural net
 model = Sequential()
-model.add(InputLayer(input_shape=(None, None, 1)))
+model.add(InputLayer(input_shape=(length, width, 1)))
 model.add(Conv2D(8, (3, 3), activation='relu', padding='same', strides=2))
 model.add(Conv2D(8, (3, 3), activation='relu', padding='same'))
 model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
@@ -46,7 +45,7 @@ model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
 model.compile(optimizer='rmsprop',loss='mse')
 
 #Train the neural network
-model.fit(x=L_chan, y=ab_chan, batch_size=1, epochs=100)
+model.fit(x=L_chan, y=ab_chan, batch_size=1, epochs=1000)
 
 #final output
 print(model.evaluate(L_chan, ab_chan, batch_size=1))
@@ -55,5 +54,5 @@ output = output * 128
 canvas = np.zeros((length, width, 3))
 canvas[:,:,0] = L_chan[0][:,:,0]
 canvas[:,:,1:] = output[0]
-imsave("img_result.jpg", lab2rgb(canvas))
-imsave("img_gray_scale.jpg", rgb2gray(lab2rgb(canvas)))
+imsave("img_result.png", lab2rgb(canvas))
+imsave("img_gray_scale.png", rgb2gray(lab2rgb(canvas)))
