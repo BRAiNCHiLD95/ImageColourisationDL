528e4cacf111e336e0a9748ac9ee0435c30d3e5c
Brian Sam Thomas <BRAiNCHiLD95@github.com>
2018-02-04 21:08:27 +0530

    changes to the alpha network

--- alpha.py
+++ alpha.py
@@ -1,9 +1,9 @@
-'''
-cv2.imshow('title', im_name)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+# -*- coding: utf-8 -*-
+"""
+Created on Sun Feb  4 14:30:58 2018
 
-'''
+@author: BkiD
+"""
 import numpy as np
 from skimage.color import rgb2lab, lab2rgb, rgb2gray
 from skimage.io import imsave
@@ -11,22 +11,18 @@ import cv2
 from keras.layers import Conv2D, UpSampling2D, InputLayer
 from keras.models import Sequential
 
-# load image
-image = np.array(cv2.imread('test_images/im1.jpg', 1))
-cv2.imshow('title', image)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
-# Convert to LAB and split L, ab
-L_chan = rgb2lab(1.0/255*image)[:,:,0]
-ab_chan = rgb2lab(1.0/255*image)[:,:,1:]
-ab_chan = ab_chan / 128
+# Get images
+image = np.array(cv2.imread('test_images/swim.jpg', 1))
 
-#getting image dimensions and reshaping the mapped image
-length,width,chans = image.shape
-L_chan = L_chan.reshape(1, length, width, 1)
-ab_chan = ab_chan.reshape(1, length, width, 2)
+# Import map images into the lab colorspace
+X = rgb2lab(1.0/255*image)[:,:,0]
+Y = rgb2lab(1.0/255*image)[:,:,1:]
+Y = Y / 128
+X = X.reshape(1, 400, 400, 1)
+Y = Y.reshape(1, 400, 400, 2)
 
-#the alpha neural net
+
+# Building the neural network
 model = Sequential()
 model.add(InputLayer(input_shape=(None, None, 1)))
 model.add(Conv2D(8, (3, 3), activation='relu', padding='same', strides=2))
@@ -46,14 +42,14 @@ model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
 model.compile(optimizer='rmsprop',loss='mse')
 
 #Train the neural network
-model.fit(x=L_chan, y=ab_chan, batch_size=1, epochs=100)
+model.fit(x=X, y=Y, batch_size=1, epochs=1000)
+print(model.evaluate(X, Y, batch_size=1))
 
-#final output
-print(model.evaluate(L_chan, ab_chan, batch_size=1))
-output = model.predict(L_chan)
+# Output colorizations
+output = model.predict(X)
 output = output * 128
-canvas = np.zeros((length, width, 3))
-canvas[:,:,0] = L_chan[0][:,:,0]
+canvas = np.zeros((400, 400, 3))
+canvas[:,:,0] = X[0][:,:,0]
 canvas[:,:,1:] = output[0]
 imsave("img_result.jpg", lab2rgb(canvas))
-imsave("img_gray_scale.jpg", rgb2gray(lab2rgb(canvas)))
+imsave("img_gray_scale.png", rgb2gray(lab2rgb(canvas)))
