906cde4957695730e0f7976803b683dddfdec3d1
Brian Sam Thomas <BRAiNCHiLD95@github.com>
2018-02-06 23:22:20 +0530

    final alpha cv2 working(kinda)

--- alpha.py
+++ alpha.py
@@ -4,25 +4,34 @@ Created on Sun Feb  4 14:30:58 2018
 
 @author: BkiD
 """
-import numpy as np
-from skimage.color import rgb2lab, lab2rgb, rgb2gray
-from skimage.io import imsave
-import cv2
-from keras.layers import Conv2D, UpSampling2D, InputLayer
+
+from keras.preprocessing.image import  array_to_img, img_to_array, load_img
+from keras.layers import Conv2D, UpSampling2D, InputLayer, Conv2DTranspose
+from keras.layers import Activation, Dense, Dropout, Flatten
+from keras.layers.normalization import BatchNormalization
 from keras.models import Sequential
+import os
+from skimage.color import rgb2lab, lab2rgb, rgb2gray, xyz2lab
+from skimage.io import imsave
+import random
+import tensorflow as tf
+import numpy as np
+import cv2
 
-# Get images
-image = np.array(cv2.imread('test_images/swim.jpg', 1))
+image = cv2.imread("test_images/man.jpg")
+image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB).astype(np.float32)
 
-# Import map images into the lab colorspace
-X = rgb2lab(1.0/255*image)[:,:,0]
-Y = rgb2lab(1.0/255*image)[:,:,1:]
-Y = Y / 128
-X = X.reshape(1, 400, 400, 1)
-Y = Y.reshape(1, 400, 400, 2)
+image = 1.0/255*image
 
+lab = cv2.cvtColor(image, cv2.COLOR_RGB2LAB)
 
-# Building the neural network
+X = lab[:,:,0]
+Y = lab[:,:,1:]
+length, width, chans = image.shape
+Y = Y/128
+X = X.reshape(1, length, width, 1)
+Y = Y.reshape(1, length, width, 2)
+
 model = Sequential()
 model.add(InputLayer(input_shape=(None, None, 1)))
 model.add(Conv2D(8, (3, 3), activation='relu', padding='same', strides=2))
@@ -38,18 +47,19 @@ model.add(Conv2D(16, (3, 3), activation='relu', padding='same'))
 model.add(UpSampling2D((2, 2)))
 model.add(Conv2D(2, (3, 3), activation='tanh', padding='same'))
 
-# Finish model
-model.compile(optimizer='rmsprop',loss='mse')
+model.compile(optimizer='rmsprop', loss='mse', metrics=['accuracy'])
+print(model.summary())
 
-#Train the neural network
-model.fit(x=X, y=Y, batch_size=1, epochs=1000)
-print(model.evaluate(X, Y, batch_size=1))
+model.fit(x=X, y=Y, batch_size=1, epochs=50)
+print(model.evaluate(X,Y, batch_size=1))
+output = model.predict(X)
+output *= 128
 
 # Output colorizations
-output = model.predict(X)
-output = output * 128
-canvas = np.zeros((400, 400, 3))
-canvas[:,:,0] = X[0][:,:,0]
-canvas[:,:,1:] = output[0]
-imsave("img_result.jpg", lab2rgb(canvas))
-imsave("img_gray_scale.png", rgb2gray(lab2rgb(canvas)))
+cur = np.zeros((400, 400, 3))
+cur[:,:,0] = X[0][:,:,0]
+cur[:,:,1:] = output[0]
+#lab = cv2.cvtColor(cur, cv2.COLOR_LAB2RGB)
+#cv2.imwrite("color.jpg",lab)
+imsave("img_result.png", lab2rgb(cur))
+imsave("img_gray_version.png", rgb2gray(lab2rgb(cur)))
